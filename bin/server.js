bd532158266d9b38c589dba3dbf7cc7a2c1fb9f6
Pavlos Karakalidis <pavloc.kara@outlook.com>
2020-02-20 19:36:38 +0000

    Add server js and Procfle

---
+++ bin/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(process.cwd() + '/dist/theinfinitytimes'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(process.cwd() + '/dist/index.html'));
+});
